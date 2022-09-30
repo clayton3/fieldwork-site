0d3a862fb4e581a6261da0326e46fbccf89ef9b3
clayton3 <claytoncblevins@gmail.com>
2022-09-30 17:35:23 -0400

    Yet another update

--- js/content/artistContent.js
+++ js/content/artistContent.js
@@ -2,7 +2,9 @@ function artistContent(){
     var content = `
         <h2>Information about Spencer LaJoye:</h2>
         <p>
-            Spencer LaJoye is an artist that writes and performs music in the genre of folk/pop 
+            Spencer LaJoye is an artist that writes and performs music in the genre of folk/pop.
+            Spencer writes of their experiences with finding their identity and what they went through in the
+            evangelical church. Through their music Spencer is able to make you connect with their 
         </p>
     `;
 
